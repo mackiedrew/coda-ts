fa7e78058e6d4207e944c44bbbdb9076792c671b
Mackie Drew <mackiedrew@gmail.com>
2022-05-04 12:15:10 -0400

    bug: fix interfaces not exporting on build

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,25 @@
-export { WhoAmI } from './resources/account';
-export { ResourceType } from './resources/base';
-export { Coda, Coda as default } from './resources/coda';
-export {
-  Column,
-  Columns,
-  ColumnText,
-  ColumnPerson,
-  ColumnLookup,
-  ColumnNumber,
-  ColumFormat,
-  ColumnType,
-  ColumnRef,
-} from './resources/columns';
-export { ControlType, Control, ControlRef } from './resources/controls';
-export { Doc, ShareMetadata, DocSize, DocRef } from './resources/doc';
-export { Category, Docs } from './resources/docs';
-export { FolderRef } from './resources/folder';
-export { Formulas, Formula, FormulaRef } from './resources/formulas';
-export { Icon } from './resources/icon';
-export { Image } from './resources/image';
-export { Link } from './resources/link';
-export { Mutation } from './resources/mutation';
-export { Page, PageRef } from './resources/page';
-export { Pages } from './resources/pages';
-export {
-  Permissions,
-  AccessType,
-  PrincipalType,
-  PrincipalAnyone,
-  PrincipalDomain,
-  PrincipalEmail,
-  Principal,
-} from './resources/permissions';
-export { PublishInfo, PublishMode, PublishOptions } from './resources/publishing';
-export { RowRef, RowDto, RowValueFormat, CellData, RowData, Row } from './resources/row';
-export {
-  RowSortBy,
-  RowListQueryType,
-  RowListQueryById,
-  RowListQueryByName,
-  RowListQuery,
-  RowListOptions,
-  RowUpsertDto,
-  ListRowResponse,
-  Rows,
-} from './resources/rows';
-export {
-  TableType,
-  TableLayout,
-  SortDirection,
-  TableRef,
-  Sort,
-  Filter,
-  TableDto,
-  Table,
-} from './resources/table';
-export { TableListOptions, Tables } from './resources/tables';
-export {
-  ScalarValue,
-  LinkedDataType,
-  ImageStatus,
-  RichValuePrototype,
-  CurrencyValue,
-  ImageUrlValue,
-  PersonValue,
-  UrlValue,
-  RowValue,
-  RichValue,
-  CellValue,
-} from './resources/values';
-export { WorkspaceRef } from './resources/workspace';
+export * from './resources/account';
+export * from './resources/base';
+export * from './resources/coda';
+export * from './resources/columns';
+export * from './resources/controls';
+export * from './resources/doc';
+export * from './resources/docs';
+export * from './resources/folder';
+export * from './resources/formulas';
+export * from './resources/icon';
+export * from './resources/image';
+export * from './resources/link';
+export * from './resources/mutation';
+export * from './resources/page';
+export * from './resources/pages';
+export * from './resources/permissions';
+export * from './resources/publishing';
+export * from './resources/row';
+export * from './resources/rows';
+export * from './resources/table';
+export * from './resources/tables';
+export * from './resources/values';
+export * from './resources/workspace';
+
+export { Coda as default } from './resources/coda';
